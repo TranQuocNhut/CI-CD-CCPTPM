6fc31a1bc5583b764f985557e17dfd8462e4c5d2
BinTran04 <tranquocnhut2004@gmail.com>
2026-03-27 11:55:56 +0700

    update

---
+++ src/test/java/com/example/VoThaiToan/VoThaiToanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VoThaiToan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoThaiToanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
